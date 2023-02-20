bbb92c6e033e70a21aacc7c318315b32afc2d520
A-Abra <apabraham472@gmail.com>
2023-02-20 14:11:34 -0500

    Removed redundancy

--- app/src/main/java/com/example/flixsterextended/DetailActivity.kt
+++ app/src/main/java/com/example/flixsterextended/DetailActivity.kt
@@ -1,17 +1,12 @@
 package com.example.flixsterextended
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.codepath.asynchttpclient.AsyncHttpClient
-import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
-import okhttp3.Headers
-import org.json.JSONException
 
 private const val TAG = "DetailActivity"
 
